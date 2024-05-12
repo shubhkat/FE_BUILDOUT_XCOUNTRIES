d7dc9eb94c2ca0506c06e44ea9642fd900dc002a
Shubham Katakwar <shubham.katakwar.7@gmail.com>
2024-05-13 00:13:36 +0530

    buildout: xcountries

---
+++ xcountries/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Flag from "./components/Flag/Flag";
+
+function App() {
+  return (
+    <>
+      <Flag />
+    </>
+  );
+}
+
+export default React.memo(App);
---
+++ xcountries/src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import styles from "./Card.module.css"
+
+const Card = ({
+    flagURL,
+    name,
+    altName
+}) => {
+  
+//   console.log("Card.jsx Card debug {flagURL, name, altName}::", {flagURL, name, altName});
+
+  return (
+    <div className={styles.card}>
+        <img src={flagURL} alt={altName} width={100} height={100} />
+        <h2>{name}</h2>
+    </div>
+  )
+}
+
+export default Card;
