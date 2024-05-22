66bedf4bfab99fe3857d20a5a3f6a11bc56f76c0
Shubham Katakwar <shubham.katakwar.7@gmail.com>
2024-05-23 02:36:14 +0530

    removed cache-control to accomodate for test cases passing

--- xcountriessearch/src/services/services.jsx
+++ xcountriessearch/src/services/services.jsx
@@ -3,16 +3,17 @@ import axios from "axios";
 const fetchCountries = async () => {
     const url = "https://restcountries.com/v3.1/all";
     try {
-        const cachedData = localStorage.getItem('countriesData');
-        if (cachedData) {
-            return JSON.parse(cachedData);
-        }
-        const headers = {
-            'Cache-Control': 'max-age=3600', // Cache for 1 hour
-        };
-        const response = await axios.get(url, { headers });
-        localStorage.setItem('countriesData', JSON.stringify(response.data));
+        // const cachedData = localStorage.getItem('countriesData');
+        // if (cachedData) {
+        //     return JSON.parse(cachedData);
+        // }
+        // const headers = {
+        //     'Cache-Control': 'max-age=3600', // Cache for 1 hour
+        // };
+        // const response = await axios.get(url, { headers });
+        const response = await axios.get(url);
         const data = response.data;
+        // localStorage.setItem('countriesData', JSON.stringify(data));
         // console.log("services.jsx fetchCountries debug data::", data);
         return data;
     } catch (error) {
